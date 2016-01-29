6ac1111aaabf8d59b468c0d22e21546c8b1264fb
aglover1221 <alecglovercanada@gmail.com>
2016-01-29 14:27:34 -0500

    Update GameScene.swift

--- AntiqueTacToe/GameScene.swift
+++ AntiqueTacToe/GameScene.swift
@@ -1,6 +1,6 @@
 //
 //  GameScene.swift
-//  AntiqueTacToe
+//  Antique-Tac-Toe
 //
 //  Created by Glover, Alec on 2016-01-29.
 //  Copyright (c) 2016 Glover, Alec. All rights reserved.
@@ -10,36 +10,35 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        let board:[antiq] = [antiq(imageNamed: "car"),antiq(imageNamed: "car"),antiq(imageNamed: "car")
+            ,antiq(imageNamed: "car"),antiq(imageNamed: "car"),antiq(imageNamed: "car")
+            ,antiq(imageNamed: "car"),antiq(imageNamed: "car"),antiq(imageNamed: "car")]
+        for var i = 0; i<board.endIndex; ++i{
+            var hy:CGfloat  =  size.height+CGFloat(30*(i/3))
+            //board[i].runAction(SKAction.moveTo(CGPoint(hy as CGFloat),0), duration: 0))
+            
+        }
         
-        self.addChild(myLabel)
+        
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+}
+
+
+
+class antiq: SKSpriteNode {
+    let thing: SKSpriteNode = SKSpriteNode(imageNamed: "thing")
+    var img:String?
+    var id:String?
+    var keyword: [String]?
+    convenience init(imageNamed: String){
+        self.init(imageNamed: imageNamed)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
